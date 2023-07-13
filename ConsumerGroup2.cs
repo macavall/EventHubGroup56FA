f5eef48cfbb04e1426a356b675f54e0d4978fefc
Matthew Cavallo <macavall@microsoft.com>
2023-07-13 16:48:37 -0500

    One Function - per msg 5x

--- ConsumerGroup2.cs
+++ ConsumerGroup2.cs
@@ -1,22 +1,22 @@
-using System;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
+//using System;
+//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Extensions.Logging;
 
-namespace EventHubGroup56FA
-{
-    public class ConsumerGroup2
-    {
-        private readonly ILogger _logger;
+//namespace EventHubGroup56FA
+//{
+//    public class ConsumerGroup2
+//    {
+//        private readonly ILogger _logger;
 
-        public ConsumerGroup2(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<ConsumerGroup2>();
-        }
+//        public ConsumerGroup2(ILoggerFactory loggerFactory)
+//        {
+//            _logger = loggerFactory.CreateLogger<ConsumerGroup2>();
+//        }
 
-        [Function("ConsumerGroup2")]
-        public void Run([EventHubTrigger("groupeventhub", Connection = "ehconnstring", ConsumerGroup = "ConsumerGroup2", IsBatched = true)] string[] input)
-        {
-            _logger.LogInformation($"First Event Hubs triggered message: {input[0]}");
-        }
-    }
-}
+//        [Function("ConsumerGroup2")]
+//        public void Run([EventHubTrigger("groupeventhub", Connection = "ehconnstring", ConsumerGroup = "ConsumerGroup2", IsBatched = true)] string[] input)
+//        {
+//            _logger.LogInformation($"First Event Hubs triggered message: {input[0]}");
+//        }
+//    }
+//}
--- eventhubtrigger.cs
+++ eventhubtrigger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -24,23 +25,30 @@ namespace EventHubGroup56FA
         public string[] Run([EventHubTrigger("groupeventhub", Connection = "ehconnstring", ConsumerGroup = "$default", IsBatched = true)] string[] input)
         //public void Run([EventHubTrigger("groupeventhub", Connection = "ehconnstring", ConsumerGroup = "$default", IsBatched = true)] string[] input)
         {
+            // Create a List of Type String
+            var stringList = new List<string>();
 
             // Log all messages and contents
             foreach(var msg in input.ToList<string>())
             {
                 _logger.LogInformation($"First Event Hubs triggered message: {msg}");
+
+                for (int i = 0; i < arraySize; i++)
+                {
+                    stringList.Add(DateTime.UtcNow.ToString("yyyy-MM-ddThh:mm:ss"));
+                }
             }
 
-            // Create String Array for Event Hub Output
-            var myArray = new String[arraySize];
+            //// Create String Array for Event Hub Output
+            //var myArray = new String[arraySize];
 
-            // Loop through popularting myArray with DateTime
-            for(int i = 0; i < arraySize; i++)
-            {
-                myArray[i] = DateTime.UtcNow.ToString("yyyy-MM-ddThh:mm:ss");
-            }
+            //// Loop through popularting myArray with DateTime
+            //for(int i = 0; i < arraySize; i++)
+            //{
+            //    myArray[i] = DateTime.UtcNow.ToString("yyyy-MM-ddThh:mm:ss");
+            //}
 
-            return myArray;
+            return stringList.ToArray();
         }
     }
 }
